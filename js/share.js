13c3c7083798689219b5ca149b3386de4ea37a6f
kaku3 <satoshi.kaku@gmail.com>
2017-09-05 12:58:12 +0900

    js変更シェアボタン登場

--- js/share.js
+++ js/share.js
@@ -77,3 +77,42 @@ return false;
 });
 });
 
+$(function() {
+    var winWidth = $('body').outerWidth(true);
+    var footer = $('#footer');
+    var slide = $('#slide');
+    //画面下位置を取得
+    var bottomPos = $(document).height() - $(window).height() -500;
+    var showFlug = false;
+ 
+    // ウィンドウより小さかったら開く
+    panelOpen();
+    //slideを画面右外へ配置
+    $('#slide').css('margin-left', winWidth+'px');
+    $(window).scroll(function () {
+        panelOpen();
+    });
+    //閉じるボタン
+    $('#close').click(function(){
+        footer.hide();
+    });
+    //ウィンドウリサイズしたらwidth変更
+    $(window).resize(function(){
+        winWidth = $('body').outerWidth(true);
+        bottomPos = $(document).height() - $(window).height() - 500;
+    });
+ 
+    function panelOpen() {
+        if ($(this).scrollTop() >= bottomPos) {
+            if (showFlug == false) {
+                showFlug = true;
+                slide.stop().animate({'marginLeft' : '0px'}, 200);
+            }
+        } else {
+            if (showFlug) {
+                showFlug = false;
+                slide.stop().animate({'marginLeft' : winWidth+'px'}, 200);
+            }
+        }
+    }
+});
